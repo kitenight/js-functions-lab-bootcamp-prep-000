c30cb619e6baf918d86dfedd94c41383d8bc789c
Lyn <jen.l@mailfence.com>
2018-09-06 21:05:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function happyHolidaysTo (holiday, name) {
 }
 
 function holidayCountdown (days, holiday) {
- return `It's ${holiday} until ${days}!`;
+ return `It's ${holiday} days until ${days}!`;
 }
 
