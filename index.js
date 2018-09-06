0641cdb1ae98483a508476a943f6d43be6f191b6
Lyn <jen.l@mailfence.com>
2018-09-06 21:13:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function happyHolidaysTo (name) {
   return `Happy holidays, ${name}!`;
 }
 
-happyHolidayTo();
+happyHolidaysTo();
 
 function happyHolidayTo (holiday, name) { 
  return `Happy ${holiday}, ${name}!`
